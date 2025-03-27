09c799c5109a1c3c642c3ebe6e374bd2808c24b6
ji200 <ji200407@naver.com>
2025-03-27 12:32:49 +0900

    3/27수업업

---
+++ lib/presentaion/my_app.dart
@@ -0,0 +1,26 @@
+import 'package:daelim_2025/presentaion/Home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
