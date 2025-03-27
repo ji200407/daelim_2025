699b278c6e5f29ada79af93e58ac2f1b8847dd27
ji200 <ji200407@naver.com>
2025-03-27 12:33:38 +0900

    3/27

--- lib/presentaion/Home/home_screen.dart
+++ lib/presentaion/Home/home_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _count = 0;
+
+  void _handleIncreaseCount() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blue, title: Text('FlutterDemo')),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed:
+            _handleIncreaseCount, //onPressed(){_handleIncreaseCount()}   ->  onPressed()=>_handleIncreaseCount() ->현재재
+        child: Icon(Icons.add),
+      ),
+
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity, //무한(에러날수있음)
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('버튼 클릭 시 횟수 증가', style: TextStyle(fontSize: 34)),
+              Text('$_count', style: TextStyle(fontSize: 40)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
